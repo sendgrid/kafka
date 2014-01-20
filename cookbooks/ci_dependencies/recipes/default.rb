2429d82d9c171054843334395b5adc00119e94e5
Kien Pham <kien@sendgrid.com>
2014-01-20 14:15:37 -0800

    adding kafka rpm make file

--- cookbooks/ci_dependencies/recipes/default.rb
+++ cookbooks/ci_dependencies/recipes/default.rb
@@ -1,10 +1,1 @@
-#
-# Cookbook Name:: ci_dependencies
-# Recipe:: default
-#
-# Copyright (C) 2013 SendGrid
-#
-# All rights reserved - Do Not Redistribute
-#
-include_recipe 'sendgrid_application_python::build'
-
+include_recipe 'sendgrid_kafka::build'
